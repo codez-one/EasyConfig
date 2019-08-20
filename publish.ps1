73dd8491d2a60dba3057bdda31e176fdf2bbf247
Murat MERCAN <mmercan@outlook.com>
2019-08-20 12:13:57 +1000

    publish fixed version updated

--- publish.ps1
+++ publish.ps1
@@ -7,13 +7,15 @@ $aspnetfolder
 
 Set-Location -Path $aspnetfolder
 dotnet publish --output ../../artifacts/ -f netcoreapp2.2 -c Release
+Copy-Item "$dir/applicationHost.xdt" ../../artifacts/
 
 Set-Location -Path $dir
 ./nuget pack -NoPackageAnalysis
-$nupkgfilename = @(Get-Childitem -Include HealthCheck* -exclude *.nuspec)[0].Name
-$nupkgfilename
+$nupkgfilename = @(Get-Childitem -path ./* -Include health* -exclude *.nuspec)[0].Name
+"file found : $nupkgfilename"
 
-# dotnet nuget push $nupkgfilename -k [Insert-your-NugetKey-Here] -s https://www.myget.org/F/mmercan/api/v3/index.json
+dotnet nuget push $nupkgfilename -k $env:NugetKey -s https://api.nuget.org/v3/index.json
+#dotnet nuget push $nupkgfilename -k $env:NugetKey -s https://www.myget.org/F/mmercan/api/v3/index.json
 
-Move-Item HealthCheck*.nupkg ./outputs -Force
+Move-Item health*.nupkg ./outputs -Force
 Set-Location -Path $dir
