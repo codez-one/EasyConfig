6c805d22db92e48c0cee3e5e48f7b91c69f78f8b
Kirsten Kluge <live@kirk.one>
2019-11-13 22:39:51 +0100

    Update Build

--- publish.ps1
+++ publish.ps1
@@ -6,7 +6,7 @@ $nugetfolder = $dir + "/" + "src/EasyConfig.SiteExtension.NuGet"
 dotnet publish "$extensionfolder/EasyConfig.SiteExtension.csproj" --output "$nugetfolder/content/" -f netcoreapp3.0 -c Release
 Copy-Item "$nugetfolder/applicationHost.xdt" "$nugetfolder/content/"
 
-./tools/nuget pack "$nugetfolder/EasyConfig.SiteExtension.nuspec" -NoPackageAnalysis -OutputDirectory ./output -properties Configuration=Release
+dotnet pack "$extensionfolder/EasyConfig.SiteExtension.csproj" -p:NuspecProperties="version=$(GitVersion.NuGetVersion)" -o ./output -c Release --no-build
 # $nupkgfilename = @(Get-Childitem -path ./output/*.nupkg)[0].Name
 
 #dotnet nuget push $nupkgfilename -k $env:NugetKey -s https://api.nuget.org/v3/index.json
