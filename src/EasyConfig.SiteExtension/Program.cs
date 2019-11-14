e2f9f63fc06337532b572778f2dbcb8770df7e64
Kirsten Kluge <live@kirk.one>
2019-11-14 20:12:03 +0100

    Add Azure KeyVault integration
    - When a KeyVault URI is in the enviroment the vault will be used also
    - This depends on a Managed Service Idendity
    
    +semver: feature

--- src/EasyConfig.SiteExtension/Program.cs
+++ src/EasyConfig.SiteExtension/Program.cs
@@ -1,6 +1,10 @@
 namespace EasyConfig.SiteExtension
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.Azure.Services.AppAuthentication;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Configuration.AzureKeyVault;
     using Microsoft.Extensions.Hosting;
 
     public class Program
@@ -9,6 +13,36 @@ namespace EasyConfig.SiteExtension
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureAppConfiguration(
+                    (ctx, builder) =>
+                    {
+                        //Build the config from sources we have
+                        var config = builder.Build();
+
+                        // Get the uri for the Vault from configuration
+                        var keyVaultUri = config["KeyVault:Uri"];
+
+                        // Add KeyVault only if the uri is not empty
+                        if (!string.IsNullOrWhiteSpace(keyVaultUri))
+                        {
+                            //Create Managed Service Identity token provider
+                            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+                            //Create the Key Vault client
+                            var keyVaultClient = new KeyVaultClient(
+                                new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback)
+                            );
+
+                            //Add Key Vault to configuration pipeline
+                            _ = builder.AddAzureKeyVault(
+                                keyVaultUri,
+                                keyVaultClient,
+                                new DefaultKeyVaultSecretManager()
+                            );
+                        }
+                    }
+                );
     }
 }
